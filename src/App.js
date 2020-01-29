fd2303ce836ce7953890c7014350a3c7c953727a
SpencerJP <spencer_j_porteous@gmail.com>
2020-01-29 16:58:55 +1100

    .

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./App.css";
 import parser from "bbcode-to-react";
 import { Accordion } from "semantic-ui-react";
-const file = require("../defaultmotd.bbcode");
-const fileReader = require("FileReader");
-
+// const file = require("../defaultmotd.bbcode");
+var FileReader = require("FileReader"),
+  fileReader = new FileReader();
 function App() {
   return (
     <div className="App">
-      <Accordion>{parser.toReact(fileReader.readTextFile(file))}</Accordion>
+      <Accordion>
+        {parser.toReact(
+          fileReader.readTextFile(new File("./defaultmotd.bbcode"))
+        )}
+      </Accordion>
     </div>
   );
 }
