5ed94b27fff71628cfac9f3e56b3dee86a5dc5c1
Splerge <damnsplurge@gmail.com>
2020-01-29 19:35:08 +1000

    now working

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Icon from flaticon.com -- Author is Roundicons
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import React from "react";
 import "./App.css";
-import parser from "bbcode-to-react";
-import { Accordion } from "semantic-ui-react";
-// const file = require("../defaultmotd.bbcode");
-var FileReader = require("FileReader"),
-  fileReader = new FileReader();
+import parser, { Tag } from "bbcode-to-react";
+import { Accordion, Segment, Dimmer, Loader } from "semantic-ui-react";
+
+function CustomLoader() {
+  return <Segment>
+    <Dimmer active>
+      <Loader />
+    </Dimmer>
+  </Segment>
+}
+
+class NewLineTag extends Tag {
+  toReact() {
+    // using this.getComponents() to get children components.
+    return (
+      <p>{this.getComponents()}</p>
+    );
+  }
+}
+parser.registerTag('nl', NewLineTag); // replace exists tag
+
+
 function App() {
+
+  const [bbComponentParent, setBbComponentParent] = React.useState(null)
+
+  React.useEffect(() => {
+    async function fetchBbCodeAndSet() {
+      let response = await fetch("defaultmotd.bbcode")
+      let data = await response.text()
+      setBbComponentParent(parser.toReact(data))
+      return data
+    }
+    try {
+      fetchBbCodeAndSet()
+    }
+    catch (err) {
+      console.error(err)
+    }
+  }, [])
+
+
   return (
     <div className="App">
       <Accordion>
-        {parser.toReact(
-          fileReader.readTextFile(new File("./defaultmotd.bbcode"))
-        )}
+        {bbComponentParent || <CustomLoader />}
       </Accordion>
     </div>
   );
