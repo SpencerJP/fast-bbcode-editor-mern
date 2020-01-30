e8478a4dab8181c5c790066348f5cc7d6967e042
SpencerJP <spencer_j_porteous@gmail.com>
2020-01-30 15:50:30 +1100

    refactored further

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 import express from "express"
-import { join } from "path"
 import discordApi from "./api/discord"
+var session = require("express-session")
+var cookieParser = require("cookie-parser")
 var fs = require("fs")
 
 const app = express()
+app.use(cookieParser())
+app.use(
+	session({
+		secret: "test1",
+		name: "test1",
+		resave: true,
+		saveUninitialized: true,
+	})
+)
 
 app.get("/rules", async (req, res) => {
 	fs.readFile("./plaintextdata_placeholder/defaultmotd.bbcode", "utf8", function(err, data) {
@@ -17,8 +27,17 @@ app.get("/rules", async (req, res) => {
 
 app.use("/api/discord", discordApi)
 
+const root = require("path").join(__dirname, "client", "build")
+app.use(express.static(root))
 app.get("*", (req, res) => {
-	res.status(200).sendFile(join(__dirname, "index.html"))
+	if (req.session.page_views) {
+		req.session.page_views++
+		console.log("You visited this page " + req.session.page_views + " times")
+	} else {
+		req.session.page_views = 1
+		console.log("Welcome to this page for the first time!")
+	}
+	res.status(200).sendFile("index.html", { root })
 })
 
 app.listen(50451, () => {
