4a0ffe326ed860850857721f9eafa1eae30792b6
SpencerJP <spencer_j_porteous@gmail.com>
2020-02-05 15:58:53 +1100

    added loading screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,36 @@ import CenterAlignmentTag from "./components/tags/CenterAlignmentTag"
 import parser from "bbcode-to-react"
 import MOTD from "./container/MOTD"
 import CommentTag from "./components/tags/CommentTag"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { Provider } from "react-redux"
+import { CookiesProvider } from "react-cookie"
+import store from "./redux/store"
+import LoadingScreen from "./container/LoadingScreen"
 
 parser.registerTag("br", LineBreakTag) // new line tag
 parser.registerTag("cent", CenterAlignmentTag) // new line tag
 parser.registerTag("comment", CommentTag) // new line tag
 
 function App() {
-	return <MOTD parser={parser} />
+	return (
+		<Provider store={store}>
+			<CookiesProvider>
+				<Router>
+					<Switch>
+						<Route exact path="/jbmotd">
+							<MOTD parser={parser} />
+						</Route>
+						<Route exact path="/loadingscreen">
+							<LoadingScreen />
+						</Route>
+						<Route>
+							<MOTD parser={parser} />
+						</Route>
+					</Switch>
+				</Router>
+			</CookiesProvider>
+		</Provider>
+	)
 }
 
 export default App
